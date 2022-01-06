e0950cdae9f1b75988a4aeda1d868d1000b5bf5d
Marc S <39275810+P-ano-past@users.noreply.github.com>
2022-01-06 10:37:36 -0800

    styling and seperation of components

--- src/Componenets/Banner/Banner.js
+++ src/Componenets/Banner/Banner.js
@@ -1,28 +1,26 @@
 import { React } from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import Name from "./Name/Name";
 import "./Banner.css";
 
 export default function Banner() {
-  const Name = "<Marc Salaver />";
-
   return (
     <Container className="outerCont">
       <Container>
         <Row>
-          <Col />
           <Col>
-            <Col className="MNC1">
-              <Col className="myNameCont1">
-                <p className="myName">{Name}</p>
-              </Col>
-            </Col>
+            <Name />
           </Col>
-          <Col />
         </Row>
       </Container>
       <Container>
         <Row>
-          <Col>BANNER COMPONENT</Col>
+          <Col>
+            <p>BANNER COMPONENT</p>
+            <a href="https://coolors.co/ff9f1c-ffbf69-ffffff-cbf3f0-2ec4b6">
+              <p>link to color pallette</p>
+            </a>
+          </Col>
         </Row>
       </Container>
     </Container>
---
+++ src/Componenets/Banner/Name/Name.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./Name.css";
+
+export default function Name() {
+  const Name = "<Marc Salaver />";
+
+  return (
+    <Col className="MNC2">
+      <Col className="MNC1">
+        <Col className="myNameCont1">
+          <p className="myName">{Name}</p>
+        </Col>
+      </Col>
+    </Col>
+  );
+}
