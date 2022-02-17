19183c1a1aafa16e748c8659134693149967e582
Marc S <marcsalaver@gmail.com>
2022-02-16 16:41:00 -0800

    styling. saving because imagse aren't loadingproperlyu

--- src/Componenets/About/About.js
+++ src/Componenets/About/About.js
@@ -8,19 +8,26 @@ export default function About() {
     <Container className="outerCont">
       <Container className="ps">
         <Row>
-          <Col>
-            <p>
-              <h2 className="idmeTitle">About me</h2>
-            </p>
-            <p className="idme">
-              I have been involved in the world of programming since 2019. I
-              used free services like CodeAcademy and FreeCodeCamp to learn the
-              basics of web development. After picking up more skills within the
-              realm of HTML, CSS, and Javascript, I attended a coding bootcamp
-              to hone my skills further. Within this coding bootcamp, I learned
-              React and started to create projects on my own.
-            </p>
-          </Col>
+          <Row>
+            <Col>
+              <p>
+                <h2 className="idmeTitle">About</h2>
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="idme">
+                I have been involved in the world of programming since 2019. I
+                used free services like CodeAcademy and FreeCodeCamp to learn
+                the basics of web development. After picking up more skills
+                within the realm of HTML, CSS, and Javascript, I attended a
+                coding bootcamp to hone my skills further. Within this coding
+                bootcamp, I learned React and started to create projects on my
+                own.
+              </p>
+            </Col>
+          </Row>
         </Row>
       </Container>
       <Container>
